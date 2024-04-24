13537be888be0520528ca56af2c5aa4aff84b980
hslinhares <henrique.s.linhares@gmail.com>
2024-04-23 21:27:24 -0300

    ajusta codigos dos testes testhat

--- checks/rstats/testthat/setup.R
+++ checks/rstats/testthat/setup.R
@@ -2,19 +2,24 @@ suppressPackageStartupMessages(library(validate))
 library(data.table)
 library(openxlsx)
 
-#inserir dpm install no makefile
 
 exec_siafi <- fread('datapackages/siafi_2024/data/execucao.csv.gz')
 aux_agrupamento <- fread('data/aux-agrupamento-dcmefo.csv')
 
 setnames(aux_agrupamento, "elem_item_cod", "elemento_item_cod")
 
-exec_siafi_unique <- unique(exec_siafi[ ,.(elemento_item_cod), by = uo_cod])
-no_match_aux_agrupamento <- exec_siafi_unique[!aux_agrupamento, on = .(uo_cod, elemento_item_cod)]
+aux_agrupamento <- unique(aux_agrupamento[,.(uo_cod, elemento_item_cod, agrupamento_dcmefo)])
 
-nrow_exec_siafi <- nrow(exec_siafi)
+exec_siafi_merged <- merge(exec_siafi, aux_agrupamento, by = c("uo_cod", "elemento_item_cod")
+                    , all.x = TRUE)
+
+exec_siafi_unique <- unique(exec_siafi_merged[ ,.(uo_cod, elemento_item_cod), by = agrupamento_dcmefo])
+no_match_aux_agrupamento <- exec_siafi_unique[is.na(agrupamento_dcmefo), ]
+
+nrow_exec_siafi <- nrow(exec_siafi_unique)
+nrow_outros <- nrow(exec_siafi_unique[agrupamento_dcmefo=="Outros",])
 nrow_no_match <- nrow(no_match_aux_agrupamento)
-nrow_outros <- nrow(aux_agrupamento[agrupamento_dcmefo == 'Outros'])
+
 ratio_outros <- nrow_outros/nrow_exec_siafi
 
 
--- checks/rstats/testthat/test_ratio_outros.R
+++ checks/rstats/testthat/test_ratio_outros.R
@@ -1,7 +1,7 @@
 test_that("ratio_outros"){
   expect_less_than(ratio_outros, 0.20)
   
-  if (ratio_outros >= 0){
+  if (ratio_outros >= 0.20){
      msg <- "agrupamento Outros acima de 20% na base execução"
      logger::log_warn(msg)
   }
