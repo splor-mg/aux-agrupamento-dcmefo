51b447628dc2f5f0001ac9b688534d31a3654e95
hslinhares <henrique.s.linhares@gmail.com>
2024-05-03 16:44:32 -0300

    cria função com parâmentro stop_on_failure para modular a paralisação dos testes no uso do testthat

--- checks/rstats/testthat/setup.R
+++ checks/rstats/testthat/setup.R
@@ -1,25 +1,8 @@
-suppressPackageStartupMessages(library(validate))
 library(data.table)
 library(openxlsx)
+library(validate)
+library(dplyr)
+library(testthat)
 
 exec_siafi <- fread(here::here('datapackages/siafi_2024/data/execucao.csv.gz'))
 aux_agrupamento <- fread(here::here('data/aux-agrupamento-dcmefo.csv'))
-
-setnames(aux_agrupamento, "elem_item_cod", "elemento_item_cod")
-
-aux_agrupamento <- unique(aux_agrupamento[,.(uo_cod, elemento_item_cod, agrupamento_dcmefo)])
-
-exec_siafi_merged <- merge(exec_siafi, aux_agrupamento, by = c("uo_cod", "elemento_item_cod")
-                    , all.x = TRUE)
-
-exec_siafi_unique <- unique(exec_siafi_merged[ ,.(uo_cod, elemento_item_cod), by = agrupamento_dcmefo])
-no_match_aux_agrupamento <- exec_siafi_unique[is.na(agrupamento_dcmefo), ]
-
-nrow_exec_siafi <- nrow(exec_siafi_unique)
-nrow_outros <- nrow(exec_siafi_unique[agrupamento_dcmefo=="Outros",])
-nrow_no_match <- nrow(no_match_aux_agrupamento)
-
-ratio_outros <- nrow_outros/nrow_exec_siafi
-
-
-
